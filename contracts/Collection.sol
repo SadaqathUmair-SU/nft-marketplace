2ed5010ac7d2044725df5a5c321279cc49ee85fb
Sadaqath Umair <63862344+SadaqathUmair-SU@users.noreply.github.com>
2022-06-13 21:21:00 +0530

    Update Collection.sol

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -1,3 +1,5 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
