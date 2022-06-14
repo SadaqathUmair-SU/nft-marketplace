5454556e8932887bf71c567756df7c4f613ab98d
Sadaqath Umair <63862344+SadaqathUmair-SU@users.noreply.github.com>
2022-06-14 23:15:17 +0530

    Update Collection.sol

--- contracts/Collection.sol
+++ contracts/Collection.sol
@@ -15,7 +15,7 @@ contract Collection is ERC721URIStorage, Ownable {
     mapping(string => uint8) private URIMapping;
     uint256 public PRICE_PER_TOKEN = 0.01 ether; //Price of each token
     uint256 public LIMIT_PER_ADDRESS = 2; //Number of NFT's that can be minted per address
-    uint256 public MAX_SUPPLY  = 5; //Max no of total suppy of NFT's
+    uint256 public MAX_SUPPLY  = 5; //Maximum supply of NFT's
 
 
     constructor() ERC721("Collection", "NFT") {}
